fd0eaa2ae044fdc73618dd1b6795d1bd7f850569
张小伟 <1824496534@qq.com>
2016-12-28 17:38:05 +0800

    第一次初始化项目（明天好的开始，加油）

---
+++ LiveStreaming/LiveStreaming/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveStreaming
+//
+//  Created by 李保东 on 16/12/28.
+//  Copyright © 2016年 李保东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveStreaming/LiveStreaming/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveStreaming
+//
+//  Created by 李保东 on 16/12/28.
+//  Copyright © 2016年 李保东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
